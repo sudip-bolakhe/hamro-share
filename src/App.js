ee1133fea8743d6359deaada7508a0763536c1f4
Sudip Bolakhe <sudipbolakhe08@gmail.com>
2023-09-05 08:16:04 +0545

    Login features

--- src/App.js
+++ src/App.js
@@ -1,16 +1,40 @@
+import { useEffect, useState } from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import UserMenu from "./layout/adminMenu";
 import Footer from "./layout/footer";
 import Header from "./layout/header";
+import EditUser from "./pages/editUser";
 import Home from "./pages/home";
+import ListUser from "./pages/listUser";
 import Login from "./pages/login";
+import SignUp from "./pages/signup";
+import { getToken } from "./service/authUtil";
 
 function App() {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  useEffect(() => {
+    // Check if a token exists in local storage
+
+    // You may need to validate the token's authenticity here
+    // For example, you can send it to your server for validation
+
+    if (getToken()) {
+      setIsLoggedIn(true);
+    } else {
+      setIsLoggedIn(false);
+    }
+  }, []);
+
   return (
     <BrowserRouter>
-      <Header />
+      {isLoggedIn ? <UserMenu /> : <Header />}
       <Routes>
         <Route path="/" element={<Home />}></Route>
         <Route path="/login" element={<Login />}></Route>
+        <Route path="/users" element={<ListUser />}></Route>
+        <Route path="/signup" element={<SignUp />}></Route>
+        <Route path="/users/edit/:id" element={<EditUser />}></Route>
       </Routes>
       <Footer />
     </BrowserRouter>
--- src/pages/login.js
+++ src/pages/login.js
@@ -1,5 +1,106 @@
+import Box from "@mui/material/Box";
+import Button from "@mui/material/Button";
+import Checkbox from "@mui/material/Checkbox";
+import Container from "@mui/material/Container";
+import FormControlLabel from "@mui/material/FormControlLabel";
+import Grid from "@mui/material/Grid";
+import Link from "@mui/material/Link";
+import TextField from "@mui/material/TextField";
+import Typography from "@mui/material/Typography";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { setToken } from "../service/authUtil";
+import { handleLogin } from "../service/rest-api";
+
 const Login = () => {
-  return "Login page";
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
+  const navigate = useNavigate();
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    handleLogin(email, password)
+      .then((data) => {
+        setToken(data.data.token);
+        navigate("/users");
+        navigate(0);
+      })
+      .catch((error) => {
+        setError(error.response.data.message);
+      });
+  };
+
+  return (
+    <Container component="main" maxWidth="sm">
+      <Box
+        sx={{
+          boxShadow: 3,
+          borderRadius: 2,
+          px: 4,
+          py: 6,
+          marginTop: 8,
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+        }}
+      >
+        <Typography component="h1" variant="h5">
+          Sign in
+        </Typography>
+        <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
+          <TextField
+            margin="normal"
+            required
+            fullWidth
+            id="email"
+            label="Email Address"
+            name="email"
+            value={email}
+            autoComplete="email"
+            onChange={(e) => setEmail(e.target.value)}
+            autoFocus
+          />
+          <TextField
+            margin="normal"
+            required
+            fullWidth
+            name="password"
+            label="Password"
+            type="password"
+            id="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            autoComplete="current-password"
+          />
+          <FormControlLabel
+            control={<Checkbox value="remember" color="primary" />}
+            label="Remember me"
+          />
+          <Button
+            type="submit"
+            fullWidth
+            variant="contained"
+            sx={{ mt: 3, mb: 2 }}
+          >
+            Sign In
+          </Button>
+          <div className="error">{error}</div>
+          <Grid container>
+            <Grid item xs>
+              <Link href="#" variant="body2">
+                Forgot password?
+              </Link>
+            </Grid>
+            <Grid item>
+              <Link href="signup" variant="body2">
+                {"Don't have an account? Sign Up"}
+              </Link>
+            </Grid>
+          </Grid>
+        </Box>
+      </Box>
+    </Container>
+  );
 };
 
 export default Login;
